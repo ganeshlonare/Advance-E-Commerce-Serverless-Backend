4997decef12725ceb51f2451735982f287f6252f
Ganesh Lonare <ganeshlonare311@gmail.com>
2024-07-05 20:13:40 +0530

    Add sign up and sign in route

---
+++ prisma/migrations/20240705134014_init_schema/migration.sql
@@ -0,0 +1,104 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `role` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `CartItem` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Category` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Image` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Inventory` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Order` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `OrderProduct` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Product` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Review` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Tag` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Wishlist` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_ProductTags` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Image" DROP CONSTRAINT "Image_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Inventory" DROP CONSTRAINT "Inventory_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderProduct" DROP CONSTRAINT "OrderProduct_orderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderProduct" DROP CONSTRAINT "OrderProduct_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_categoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Review" DROP CONSTRAINT "Review_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Review" DROP CONSTRAINT "Review_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Wishlist" DROP CONSTRAINT "Wishlist_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Wishlist" DROP CONSTRAINT "Wishlist_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_ProductTags" DROP CONSTRAINT "_ProductTags_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_ProductTags" DROP CONSTRAINT "_ProductTags_B_fkey";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt",
+DROP COLUMN "role",
+DROP COLUMN "updatedAt";
+
+-- DropTable
+DROP TABLE "CartItem";
+
+-- DropTable
+DROP TABLE "Category";
+
+-- DropTable
+DROP TABLE "Image";
+
+-- DropTable
+DROP TABLE "Inventory";
+
+-- DropTable
+DROP TABLE "Order";
+
+-- DropTable
+DROP TABLE "OrderProduct";
+
+-- DropTable
+DROP TABLE "Product";
+
+-- DropTable
+DROP TABLE "Review";
+
+-- DropTable
+DROP TABLE "Tag";
+
+-- DropTable
+DROP TABLE "Wishlist";
+
+-- DropTable
+DROP TABLE "_ProductTags";
+
+-- DropEnum
+DROP TYPE "OrderStatus";
+
+-- DropEnum
+DROP TYPE "Role";
--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,69 @@ const app = new Hono<{
   }
 }>()
 
-app.all('/api/signup',async (c)=>{
+app.post('/api/signup',async (c)=>{
   const body = await c.req.json()  
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
+  
+  try {
+    const user = await prisma.user.create({
+      data:{
+        name:body.name,
+        email:body.email,
+        password:body.password
+      }
+    })
 
+    return c.json({
+      success : true,
+      message:"user registered successfully",
+      user
+    })
+  } catch (error : any) {
+    console.log("error in signup")
+    console.log(error.message)
+    return c.json({
+      success : false,
+      message:error.message
+    })
+  }
+})
+
+app.post('/api/signin', async(c)=>{
+  const body = await c.req.json()
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    try {
+      const {email , password} = body
+      const user = await prisma.user.findUnique({
+        where:{
+          email:email
+        }
+      })
+      if(!user){
+        return c.json({
+          success : false,
+          message:"user not found"
+        })
+      }
+
+      return c.json({
+        success:true,
+        message:"user logged in successfully",
+        user
+      })
+    } catch (error:any) {
+      console.log("error in signin")
+      console.log(error.message)
+      return c.json({
+      success : false,
+      message:error.message
+      })
+    }
 })
 
 app.get('/', (c) => {
