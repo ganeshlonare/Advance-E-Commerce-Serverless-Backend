4eb49a4ba8468021b6206316463bd8f408cc90e2
Ganesh Lonare <ganeshlonare311@gmail.com>
2024-07-05 18:38:20 +0530

    Added @prisma/extension-accelerate dependency, updated Prisma client and schema, and modified index.ts to use PrismaClient with accelerate extension. Also updated wrangler.toml with DATABASE_URL variable.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:string
+  }
+}>()
+
+app.all('/api/signup',async (c)=>{
+  const body = await c.req.json()  
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+})
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-export default app
+export default app  
